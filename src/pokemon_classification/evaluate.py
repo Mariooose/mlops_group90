3e95c0756815ccec010a90de3d631957873d7c15
Mathilde Philbert <mathildenielsen@mathildes-mac.students.clients.local>
2025-01-15 14:02:53 +0100

    evaluate

--- src/pokemon_classification/evaluate.py
+++ src/pokemon_classification/evaluate.py
@@ -1,7 +1,7 @@
 import torch
 import typer
-from pokemon_classification.data import corrupt_mnist
-from pokemon_classification.model import MyAwesomeModel
+from data import pokemon_data
+from model import MyAwesomeModel
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
@@ -14,8 +14,8 @@ def evaluate(model_checkpoint: str) -> None:
     model = MyAwesomeModel().to(DEVICE)
     model.load_state_dict(torch.load(model_checkpoint))
 
-    _, test_set = corrupt_mnist()
-    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=50)
+    _, test_set = pokemon_data()
+    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=32)
 
     model.eval()
     correct, total = 0, 0
